df7f6197ba1d28a6ae30c2094e7e1ef0e1086348
mostertjc <102650119+mostertjc@users.noreply.github.com>
2022-11-16 02:57:54 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+let x = 0
+let y = 0
+basic.forever(function () {
+    x = input.acceleration(Dimension.X)
+    y = input.acceleration(Dimension.X)
+    if ((33 as any) > (32 as any)) {
+        basic.showIcon(IconNames.Heart)
+    }
+})
